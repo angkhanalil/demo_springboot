d8126e9081dc429cc416ccc8958b4b77ea491881
angkhanalil <angkhana.lil94@gmail.com>
2021-10-19 16:31:41 +0700

    add geo

--- src/main/java/com/example/demo/dto/GeoDTO.java
+++ src/main/java/com/example/demo/dto/GeoDTO.java
@@ -1,5 +1,21 @@
 package com.example.demo.dto;
 
 public class GeoDTO {
+
+    String lat;
+    String lng;
+    public String getLat() {
+        return lat;
+    }
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+    public String getLng() {
+        return lng;
+    }
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
     
 }
